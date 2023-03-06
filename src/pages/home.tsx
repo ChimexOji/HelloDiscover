856aa0de61b8e992705ec40db036803341c2a315
Chimezie Oji <chimezieoji@yahoo.com>
2023-03-06 15:23:27 +0100

    first commit

---
+++ src/pages/home.tsx
@@ -0,0 +1,22 @@
+import { useList } from '@pankod/refine-core';
+import { Typography, Box, Stack } from '@pankod/refine-mui';
+
+import {
+  PieChart,
+  PropertyReferrals,
+  TotalRevenue,
+  PropertyCard,
+  TopAgent
+} from 'components';
+
+const home = () => {
+  return (
+    <Box>
+      <Typography fontSize={25} fontWeight={700} color='#11142D'>
+        Dashboard
+      </Typography>
+    </Box>
+  )
+}
+
+export default home
